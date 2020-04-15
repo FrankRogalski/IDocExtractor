d75a015f57d7d84fed9f09d915965a5bc3083ea5
frankrogalski <frank.rogalski@hansa-flex.com>
2020-04-15 13:38:18 +0200

    implemented argparse

--- IDocExtractor.py
+++ IDocExtractor.py
@@ -1,20 +1,28 @@
-from sys import argv
+import argparse
 import re
 
-if len(argv) < 4:
-    print("Please specify at least 3 arguments Sourcefile, Targetfile and IDoc types. {} where given".format(len(argv) - 1))
-    exit(1)
+parser = argparse.ArgumentParser(description='Create a file of changed Customers from three unfiltered xls files')
 
-pattern_string = r'(<\?xml version="1.0" encoding="UTF-8"\?><(' + r"|".join(argv[3:]) + r')>.*</\2>)'
-pattern = re.compile(pattern_string)
+parser.add_argument("-s","--source", dest="source_path", help="The Datahub log from which this programm will retrieve the IDocs", required=True)
+parser.add_argument("-t", "--target", dest="target_path", help="The file which will contain the IDocs", required=True)
+parser.add_argument("-it", "--idoc-types", dest="idoc_types", nargs="+", help="The IDoc types that will be filtered", required=True)
 
-try:
-    with open(argv[1], "r") as source:
-        with open(argv[2], "w") as target:
-            for source_line in source:
-                match = pattern.search(source_line)
-                if match:
-                    target.write(match.group(1) + "\n")
-except FileNotFoundError:
-    print("The source File you specified ({}) wasn't found".format(argv[1]))
-    exit(1)
+args = parser.parse_args()
+
+def main():
+    pattern_string = r'(<\?xml version="1.0" encoding="UTF-8"\?><(' + r"|".join(args.idoc_types) + r')>.*</\2>)'
+    pattern = re.compile(pattern_string)
+
+    try:
+        with open(args.source_path, "r") as source:
+            with open(args.target_path, "w") as target:
+                for source_line in source:
+                    match = pattern.search(source_line)
+                    if match:
+                        target.write(match.group(1) + "\n")
+    except FileNotFoundError:
+        print("The source File you specified ({}) wasn't found".format(argv[1]))
+        exit(1)
+
+if __name__ == "__main__":
+    main()
